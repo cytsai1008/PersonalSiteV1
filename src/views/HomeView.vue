c9177228a37e3277907d3bfbf8ca290335e6eab6
CYTsai <cytsai1008@gmail.com>
2022-04-20 10:52:26 +0000

    Working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,9 +14,14 @@ import { Head } from "@vueuse/head";
     <TheWelcome />
     -->
     <div class="ts-center">
+      <!--
+      <div class="ts-image is-circular is-small">
+        <img src="./image.png" />
+      </div>
+      -->
       <div class="ts-header is-heavy is-massive">Hi, I'm Saito Kenichi</div>
       <div class="ts-header is-heavy is-big">
-        A high school student / Programmer
+        A high school student / Amateur programmer
       </div>
     </div>
   </main>
