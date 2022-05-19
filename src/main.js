28e76be0c147fb029a59dda6bb7d27d644f52a42
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-05-20 00:33:16 +0800

    Remaking

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createHead } from "@vueuse/head";
 
 import App from "./App.vue";
 import router from "./router";
+import "./assets/tailwind.css";
 
 const app = createApp(App);
 const head = createHead();
